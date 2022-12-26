091fe1ad86f1b1feab9d3595a300969667d00a2f
riyan-eng <fbriyant11@gmail.com>
2022-12-26 19:59:56 +0700

    tipe data number

---
+++ 2_tipedatanumber/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// tipe data integer
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+
+	// tipe data floating point
+	fmt.Println("Tiga koma lima = ", 3.5)
+
+	// alias
+}
