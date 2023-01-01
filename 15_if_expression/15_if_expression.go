cb310d53de89d1b4f1a7d5544375d6f0179b1be0
riyan-eng <fbriyant11@gmail.com>
2023-01-01 19:29:11 +0700

    if statement

--- 15_if_expression/15_if_expression.go
+++ 15_if_expression/15_if_expression.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func main() {
+	var name = "Febriss"
+
+	if name == "Riyan" {
+		fmt.Println("Halo Riyan")
+	} else if name == "Febri" {
+		fmt.Println("Halo Febri")
+	} else {
+		fmt.Println("Hi kenalan dong")
+	}
+
+	//
+	if length := len(name); length > 5 {
+		fmt.Println("Terlalu panjang")
+	} else {
+		fmt.Println("Nama sudah benar")
+	}
+}
