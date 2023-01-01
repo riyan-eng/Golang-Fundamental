7391c29e273e990edf829984296b1b2ce555f12c
riyan-eng <fbriyant11@gmail.com>
2023-01-01 20:26:22 +0700

    break & continue

---
+++ 18_break_and_continue/18_break_and_continue.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		if i == 5 {
+			break
+		}
+		fmt.Println("Perulangan ke", i)
+	}
+
+	//
+	for i := 1; i <= 10; i++ {
+		if i == 5 {
+			continue
+		}
+		fmt.Println("Perulangan ke", i)
+	}
+
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("Perulangan ke", i)
+	}
+}
