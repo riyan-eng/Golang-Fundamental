1baebda7d4109f7d008f9263424d0accf309ce62
riyan-eng <fbriyant11@gmail.com>
2023-01-04 21:05:27 +0700

    interface

--- 35_interface/35_interface2.go
+++ 35_interface/35_interface2.go
@@ -10,8 +10,16 @@ type Kucing struct {
 	Name string
 }
 
+type Anjing struct {
+	Name string
+}
+
 func (kucing Kucing) getSuara(suara string) string {
 	return suara + " " + kucing.Name
+}
+
+func (anjing Anjing) getSuara(suara string) string {
+	return suara + " " + anjing.Name
 }
 
 func main() {
@@ -19,10 +27,11 @@ func main() {
 	// memanggil singlw data
 	riyan := Kucing{"riyan"}
 	feri := Kucing{"feri"}
+	rizal := Kucing{"rizal"}
 	fmt.Println(riyan.getSuara("aum"))
 
 	// memanggul dengan iterasi
-	binatang := []Binatang{riyan, feri}
+	binatang := []Binatang{riyan, feri, rizal}
 	for _, bin := range binatang {
 		fmt.Println(bin.getSuara("rarr"))
 	}
